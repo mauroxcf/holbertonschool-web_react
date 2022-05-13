eafa8e5049184d4c73ba1d4b3ccce060d29f2152
mauroxcf <maurox03@gmail.com>
2022-05-13 00:00:00 -0500

    task 3

---
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: {
+    header: './modules/header/header.js',
+    body: './modules/body/body.js',
+    footer: './modules/footer/footer.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin()],
+  devServer: {
+    static: path.join(__dirname, './public'),
+    compress: true,
+    port: 8564,
+  },
+  optimization: {
+    runtimeChunk: 'all'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  }
+};
