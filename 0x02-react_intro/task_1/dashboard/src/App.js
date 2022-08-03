5c63892dc1a938bbaef53b527977ef5ee2429cb9
mauroxcf <maurox03@gmail.com>
2022-08-03 03:21:34 -0500

    init task 1

---
+++ 0x02-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import logo from "./logo.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <footer className="App-footer">Copyright 2020 - holberton School</footer>
+    </div>
+  );
+}
+
+export default App;
