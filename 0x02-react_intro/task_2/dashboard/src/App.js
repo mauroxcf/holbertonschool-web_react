9592751820af201a7070cbf89a5f3707a4400fba
mauroxcf <maurox03@gmail.com>
2022-08-03 18:48:27 -0500

    finish task 2 form

---
+++ 0x02-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import logo from "./logo.jpg";
+import { getFooterCopy, getFullYear } from "./utils";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <form>
+          <label>
+            email: <input type="text" name="email" />
+          </label>
+          <label>
+            password: <input type="text" name="password" />
+          </label>
+          <button type="submit" value="submit">
+            OK
+          </button>
+        </form>
+      </div>
+      <footer className="App-footer">
+        Copyright {getFullYear()} - {getFooterCopy(true)}
+      </footer>
+    </div>
+  );
+}
+
+export default App;
