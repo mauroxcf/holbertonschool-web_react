851d079b669edddcc7a29eac6ac1e0988955bf08
mauroxcf <maurox03@gmail.com>
2022-08-09 18:21:35 -0500

    task 0 improvement

--- 0x04-React_component/task_0/dashboard/src/App/App.js
+++ 0x04-React_component/task_0/dashboard/src/App/App.js
@@ -8,25 +8,26 @@ import CourseList from "../CourseList/CourseList";
 import { getLatestNotification } from "../utils";
 import React from "react";
 
+const listCourses = [
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 },
+];
+
+const listNotifications = [
+  { id: 1, type: "default", value: "New course available" },
+  { id: 2, type: "urgent", value: "New resume available" },
+  {
+    id: 3,
+    type: "urgent",
+    html: {
+      __html: getLatestNotification(),
+    },
+  },
+];
+
 export default class App extends React.Component {
   render() {
-    const listCourses = [
-      { id: 1, name: "ES6", credit: 60 },
-      { id: 2, name: "Webpack", credit: 20 },
-      { id: 3, name: "React", credit: 40 },
-    ];
-
-    const listNotifications = [
-      { id: 1, type: "default", value: "New course available" },
-      { id: 2, type: "urgent", value: "New resume available" },
-      {
-        id: 3,
-        type: "urgent",
-        html: {
-          __html: getLatestNotification(),
-        },
-      },
-    ];
     const { isLoggedIn } = this.props;
     return (
       <>
